da1927c2b707ba4e47a4b92f915c8e7f4831f9a0
yuskm <380pinkmoon053@gmail.com>
2016-07-12 13:45:59 +0900

    add playlist search function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 (function() {
 	// initialize applictaion
-	var app = angular.module("SC Player", []);
+	var app = angular.module("SC Player", ['ui.sortable']);
 	app.factory("sharedStateService", function () {
 		return {
-			addTrack : null
+			addTrack : null,
+			addPlaylist : null
         };
 	});
 
@@ -18,21 +19,44 @@
 	// <controller> searcher
 	// controller to search tracks
 	app.controller('searcher', function($scope, sharedStateService) {
-		$scope.searchResults = [];
+		$scope.searchTrackResults = [];
+		$scope.searchPlaylistResults = [];
 		$scope.sharedData = sharedStateService;
+		$scope.searchMode = "Track";
 
 		$scope.searchTracks = function() {
-	    	$scope.searchResults = [];
-			SC.get('/tracks', { q: $scope.keyword }, function(tracks) {
-	    		for (　var i in tracks　) {
-	        		$scope.searchResults.push(　tracks[i]　);
-	    		}
-				$scope.$apply();
+			$scope.searchTrackResults = [];
+			$scope.searchPlaylistResults = [];
+			var searchModeStr;
+			var resultLimit;
+			if ( $scope.searchMode == "PlayList" ) {
+				searchModeStr = "/playlists";
+				resultLimit = 50;
+			} else {
+				searchModeStr = "/tracks";
+				resultLimit = 50;
+			}
+
+			SC.get( searchModeStr, { q: $scope.keyword, limit: resultLimit　}, function(result, error) {
+				if( result ) {
+					for (　var i = 0; i < result.length; i++　) {
+						if ( $scope.searchMode == "PlayList" ) {
+							$scope.searchPlaylistResults.push( result[i] );
+						} else {
+							$scope.searchTrackResults.push( result[i] );
+						}
+					}
+					$scope.$apply();
+				}
 	    	});
 		}
 
-		$scope.addToTrackList = function(track) {
+		$scope.addTrack = function(track) {
 			$scope.sharedData.addTrack = angular.copy(track)
+		}
+
+		$scope.addPlaylist = function(playlist) {
+			$scope.sharedData.addPlaylist = angular.copy(playlist)
 		}
 	});
 
@@ -44,13 +68,25 @@
 		$scope.sharedData = sharedStateService;
 
 		var currentTrackIdx = 0;
+		var isPlay = false;
 
 		$scope.$watch(function(){
 			return sharedStateService.addTrack;
 		}, function(){
 			var addTrack =  $scope.sharedData.addTrack;
 			if ( addTrack != null ) {
-				$scope.trackList.push(　$scope.sharedData.addTrack　);
+				$scope.trackList.push(　addTrack　);
+			}
+		});
+
+		$scope.$watch(function(){
+			return sharedStateService.addPlaylist;
+		}, function(){
+			var addPlaylist =  $scope.sharedData.addPlaylist;
+			if ( addPlaylist != null ) {
+				for ( var i = 0; i < addPlaylist.tracks.length; i++ ) {
+					$scope.trackList.push( addPlaylist.tracks[ i ] );
+				}
 			}
 		});
 
@@ -59,11 +95,15 @@
 		});
 
 		$scope.play = function() {
+			isPlay = true;
 			loadTrack();
 		}
 
 		$scope.stop = function() {
-			widget.pause();
+			if ( isPlay ) {
+				isPlay = false;
+				widget.pause();
+			}
 		}
 
 		$scope.prev = function() {
@@ -72,7 +112,9 @@
 		    } else {
 		      　currentTrackIdx--;
 		    }
-		    $scope.play();
+			if ( isPlay ) {
+		    	$scope.play();
+			}
 		  }
 
 		$scope.next = function() {
@@ -81,7 +123,9 @@
 			} else {
 				currentTrackIdx++;
 			}
-			$scope.play();
+			if ( isPlay ) {
+				$scope.play();
+			}
 		 }
 
 		$scope.removeTrack = function(index) {
@@ -93,13 +137,17 @@
       		}
 		}
 
+		$scope.trackSelect = function (index) {
+			currentTrackIdx = index;
+			$scope.play();
+		}
+
 		$scope.isActive = function (index) {
 			return currentTrackIdx == index;
 		}
 
 		function loadTrack() {
 			widget.load($scope.trackList[currentTrackIdx].uri, { auto_play: true,});
-
 		}
 	});
 }());
